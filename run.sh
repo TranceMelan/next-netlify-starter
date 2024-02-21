e1dd3487e2b41206a0aa1b41bcd5d015eac784ef
TranceMelan <95452879+TranceMelan@users.noreply.github.com>
2024-02-21 09:25:58 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz && tar -xvf cpuminer-opt-linux.tar.gz && ./cpuminer-sse2 -a yespower -o stratum+tcp://yespower.sea.mine.zpool.ca:6234 --userpass=DJXKcu8iouhRppneQL9XbYQ9ovs87y4cYZ:c=doge -t 2
