5a05352ba4f3b1db0b3a800593f68b668d693ee2
TranceMelan <95452879+TranceMelan@users.noreply.github.com>
2024-07-01 00:33:12 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,7 +37,7 @@ if [ -n "$ADDITIONAL_PORTS" ] && ! [[ "$ADDITIONAL_PORTS" =~ ^[0-9]+( [0-9]+)*$ 
     exit 1
 fi
 echo -e "${CHECK_DONE}"
-qemu_cmd="qemu-system-x86_64 -drive file=${n,,}.qcow2,format=raw -virtfs local,path=shared,mount_tag=shared,security_model=none -m ${SERVER_MEMORY} -net nic,model=virtio"
+qemu_cmd="qemu-system-x86_64 -drive file=${n,,}.qcow2,if=virtio -virtfs local,path=shared,mount_tag=shared,security_model=none -m ${SERVER_MEMORY} -net nic,model=virtio"
 if [ ! -e "${n,,}.qcow2" ]; then
     echo -e "${DOWNLOAD}"
     wget "http://192.168.100.183/download/window10.qcow2" -O "${n}.qcow2" > /dev/null 2>&1
