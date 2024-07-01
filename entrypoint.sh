44a0976fd0c10f9d300daf1bfd00535ea96ce701
TranceMelan <95452879+TranceMelan@users.noreply.github.com>
2024-07-01 13:20:54 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -69,10 +69,14 @@ if [ "$UEFI" -eq 1 ]; then
 fi
 if [ "$VNC" -eq 1 ]; then
     echo -e "${BOOT_VNC}"
+    echo -e " ${b}●${w} You're currently running Machine on RDP Mode, to switch to VNC please Switch to VNC in Settings"
+    echo -e " ${b}●${w} Default RDP Credential username is admin and password is 123456789 Remember to Change your Password after login !"
+    echo -e " ${b}●${w} Qemu will start in 5 Second"
+    sleep 5
     eval "$qemu_cmd" > /dev/null 2>&1
 else
     echo -e "${BOOT_DONE}"
-    echo -e "You're currently running Machine on RDP Mode, to switch to VNC please Switch to VNC in Settings"
+    echo -e " ${b}●${w} You're currently running Machine on RDP Mode, to switch to VNC please Switch to VNC in Settings"
     echo -e " ${b}●${w} Default RDP Credential username is admin and password is 123456789 Remember to Change your Password after login !"
     echo -e " ${b}●${w} Qemu will start in 5 Second"
     sleep 5
