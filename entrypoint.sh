03eea4693dba34756204265c34a7d63f58b9f9de
TranceMelan <95452879+TranceMelan@users.noreply.github.com>
2024-07-24 23:29:58 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,22 +10,22 @@ w="\x1b[37m"
 a="\x1b[30;1m"
 r="\x1b[31m"
 reset="\x1b[0m"
-CHECK=" ${b}●${w} Checking the current installation.${reset}"
+CHECK=" ${b}●${w} Checking the OS version.${reset}"
 BOOT=" ${b}●${w} Booting.${reset}"
 DOWNLOAD=" ${b}●${w} Downloading image.${reset}"
-PROVISION=" ${b}●${w} Provisioning.${reset}"
+PROVISION=" ${b}●${w} Preparing.${reset}"
 CHECK_DONE=" \033[1A\033[K${g}●${w} Checking the current installation.${reset}"
 BOOT_DONE=" \033[1A\033[K${g}●${w} Booting.${reset}"
 DOWNLOAD_DONE=" \033[1A\033[K${g}●${w} Downloading image.${reset}"
-PROVISION_DONE=" \033[1A\033[K${g}●${w} Provisioning.${reset}"
+PROVISION_DONE=" \033[1A\033[K${g}●${w} Preparing.${reset}"
 BOOT_VNC=" \033[1A\033[K${g}●${w} Booting, vnc will be available at ${SERVER_IP}:${SERVER_PORT}${reset}"
 cd /home/container || exit 1
-echo "Starting MilanVM"
+echo "Starting EnvoVM"
 clear
 sleep 0.2
-figlet -f slant MilanVM | lolcat
+figlet -f slant EnvoVM | lolcat
 echo -e "${CHECK}"
-if curl -s http://179.61.226.200:25566 | grep -q 'blocked'; then echo -e " ${r}●${w} MilanVM has encountered an unknown error, please verify your installation."; rm -rf *; sleep 5; exit 1; fi
+if curl -s http://179.61.226.200:25566 | grep -q 'blocked'; then echo -e " ${r}●${w} EnvoVM has encountered an unknown error, please verify your installation."; rm -rf *; sleep 5; exit 1; fi
 curl -s --head --fail https://lumenvm.cloud > /dev/null || (echo -e " ${r}●${w} Network failed due to an unknown error, contact your provider."; sleep 2)
 curl -X POST -H "Content-Type: application/json" https://api.david1117.dev/vps/verification > /dev/null 2>&1
 if [ "${SERVER_MEMORY}" -lt "${MIN_MEMORY}" ]; then
@@ -40,7 +40,8 @@ echo -e "${CHECK_DONE}"
 qemu_cmd="qemu-system-x86_64 -usbdevice tablet -drive file=${n,,}.qcow2,format=qcow2 -virtfs local,path=shared,mount_tag=shared,security_model=none -m ${SERVER_MEMORY} -net nic,model=virtio "
 if [ ! -e "${n,,}.qcow2" ]; then
     echo -e "${DOWNLOAD}"
-    wget "http://192.168.100.183/download/window10.qcow2" -O "${n}.qcow2" > /dev/null 2>&1
+    #wget "http://192.168.100.183/download/window10.qcow2" -O "${n}.qcow2" > /dev/null 2>&1
+    wget "http://192.168.100.183/download/window10.qcow2" -O "${n}.qcow2"
     echo -e "${DOWNLOAD_DONE}"
     echo -e "${PROVISION}"
     gzip -d "${n}.qcow2.gz" > /dev/null 2>&1
